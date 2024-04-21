b8a0e7ce55240866739b0dd9061bcca0ec02da40
beastmode900 <mjbaker3931@gmail.com>
2024-04-21 11:42:57 -0400

    added sound to secrets, started blog and game, added home button

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
-//console.log('Hello World!')
-//var topBTN = document.getElementsByClassName("topBTN");
-//topBTN.addEventListener("mouseover",  )
+let src = '../sounds/kh1fanfare.wav';
+let audio = new Audio(src);
 var secret1Found = 0;
 document.addEventListener('DOMContentLoaded', function() {
 var secretCounter = 0;
@@ -9,6 +8,7 @@ secret1.addEventListener("click", function(){
     secret1Found = isFound(secret1Found);
     if(secret1.className === "hidden"){
         secret1.className = "";
+        audio.play();
         secret1.innerHTML = "secret 1 :D <br> <small><i> you've found " + secretCounter + " secret(s)</i></small>"
     }else{
         secret1.className = "hidden";
@@ -20,6 +20,7 @@ function reveal(){
     secret1Found = isFound(secret1Found);
     if(secret1.className === "hidden"){
         secret1.className = "";
+        audio.play();
         secret1.innerHTML = secret1.innerHTML + " <br> <small> " + secretCounter + " </small>"
     }else{
         secret1.className = "hidden";
@@ -33,6 +34,11 @@ function isFound(secret){
         return 1;
     }
     return 1;
+}
+
+function playSound(src) {
+    const audio = new Audio(src);
+    audio.play();
 }
 
 
