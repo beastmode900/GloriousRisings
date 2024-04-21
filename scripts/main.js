55f17510c0fb6719d163cbce8994814d0a40c4c5
beastmode900 <mjbaker3931@gmail.com>
2024-04-21 18:39:36 -0400

    added updating secret counter

--- scripts/main.js
+++ scripts/main.js
@@ -2,28 +2,36 @@ let secretSrc = '../sounds/kh1fanfare.wav';
 let secretAudio = new Audio(secretSrc);
 let laughSrc = '../sounds/chipmunklaugh.mp3';
 let laughAudio = new Audio(laughSrc);
-var secret1Found = 0;
-document.addEventListener('DOMContentLoaded', function() {
+var clickSecretFound = 0;
+let homeSecretFound = 0;
 var secretCounter = 0;
+let secretCounterHTML = document.getElementById('secretCounter')
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
 const secret1 = document.getElementById("secret1");
 secret1.addEventListener("click", function(){
-    secret1Found = isFound(secret1Found);
+    clickSecretFound = isFound(clickSecretFound);
     if(secret1.className === "hidden"){
         secret1.className = "";
         secretAudio.play();
-        secret1.innerHTML = "secret 1 :D <br> <small><i> you've found " + secretCounter + " secret(s)</i></small>"
+      //  secret1.innerHTML = "secret 1 :D <br> <small><i> you've found " + secretCounter + " secret(s)</i></small>"
     }else{
         secret1.className = "hidden";
     }
 });
 
-function bugger(){
-    homeBTN.innerHTML = '<i>you cheeky bugger you</i>';  
-}
 
 const homeBTN =  document.getElementById("homeBTN");
 homeBTN.addEventListener("click", function(){
     laughAudio.play();
+    homeSecretFound = isFound(homeSecretFound);
     homeBTN.innerHTML = '<i>you cheeky bugger you</i>';   
     setTimeout(() => {
         homeBTN.innerHTML = 'home';
@@ -35,11 +43,11 @@ homeBTN.addEventListener("click", function(){
 
 
 function reveal(){
-    secret1Found = isFound(secret1Found);
+    clickSecretFound = isFound(clickSecretFound);
     if(secret1.className === "hidden"){
         secret1.className = "";
         audio.play();
-        secret1.innerHTML = secret1.innerHTML + " <br> <small> " + secretCounter + " </small>"
+      //  secret1.innerHTML = secret1.innerHTML + " <br> <small> " + secretCounter + " </small>"
     }else{
         secret1.className = "hidden";
     }
@@ -49,6 +57,7 @@ function reveal(){
 function isFound(secret){
     if(secret === 0){
         secretCounter++;
+        secretCounterHTML.innerHTML = 'secrets found: ' + secretCounter + "/5"
         return 1;
     }
     return 1;
