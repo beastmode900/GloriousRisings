3b31c3ca909b0c9e3daed7182ef8c77e0669a8a1
Matthew Baker <bakermj2@vcu.edu>
2024-07-16 09:28:35 -0400

    so many pictures.. almost done tho

--- public/scripts/blogGallery.js
+++ public/scripts/blogGallery.js
@@ -32,6 +32,17 @@ textBlocks = [
 "Stacie Orrico - S/T, released in 2003 this is such a sweet pop album",
 "Ostraca - Disaster",
 "in wolves clothing - it eats itself",
-"Knumears - A Shout to See"
+"Knumears - A Shout to See",
+"The Cranberries - No Need to Argue",
+"Chune - Burnt",
+"Saves the Day - Ups & Downs: Early Recordings and B-sides",
+"Hum - You'd Prefer an Astronaut",
+"Cursive - Domestica",
+"Kate Bush - Hounds of Love",
+"Carly Rae Jepsen - The Loneliest Time",
+"Joanna Newsom - Ys",
+"Blink-182 - S/T",
+"Jimmy Eat World - Static Prevails",
+"Blink-182 - Take Off Your Pants and Jacket"
 
 ]
